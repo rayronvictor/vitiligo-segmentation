74fab467af72d7a5ceeffed3a0c70742b31c1b4e
Rayron Victor <rayronaraujo@tjrn.jus.br>
2025-09-10 14:32:12 -0300

    wip

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import gradio as gr
 import PIL.Image as Image
 from ultralytics import YOLO
-from rembg import remove
+from rembg import remove, new_session
 import numpy as np
 import cv2
 
-MODEL_PATH = "runs/segment/train3/weights/best.pt"
+# MODEL_PATH = "runs/segment/train3/weights/best.pt"
+MODEL_PATH = "yolo11n-seg.pt"
 model = YOLO(MODEL_PATH)
 
 def predict_image(img, conf_threshold, iou_threshold):
@@ -25,64 +26,81 @@ def predict_image(img, conf_threshold, iou_threshold):
 
     return im
 
+def find_point_inside_sticker(image):
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+    hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
+
+    # Lower red range
+    lower_red1 = np.array([0, 120, 120])
+    upper_red1 = np.array([5, 255, 255])
+    mask1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+
+    # Upper red range
+    lower_red2 = np.array([175, 120, 120])
+    upper_red2 = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+
+    final_mask = mask1 + mask2
+
+    y_coords, x_coords = np.where(final_mask > 0)
+
+    return final_mask, int(np.mean(x_coords)), int(np.mean(y_coords))
+
+
 def calc_sticker_in_hand(input_image):
     """
     1. Upload an image of a hand with a reference sticker.
     2. Remove the image background, isolating the hand and sticker.
     3. Measure the pixel area of the hand (including the sticker).
-    4. Measure the pixel area of the sticker.
-    5. Return the sticker/hand ratio by dividing the sticker's area by the hand's area.
+    4. Find a point inside the sticker.
+    5. Segment the sticker based on the point inside the sticker
+    6. Measure the pixel area of the sticker.
+    7. Return the segmented hand and sticker images and the sticker area in hand units.
     """
 
-    # 2. Remove background from the image
-    img_without_bg_pil = remove(input_image)
-    img_without_bg = np.array(img_without_bg_pil)
+    # 2. Remove the image background, isolating the hand and sticker.
+    hand_img_without_bg_pil = remove(input_image)
+    hand_img_without_bg = np.array(hand_img_without_bg_pil)
+    hand_img_without_bg_alpha = hand_img_without_bg[:, :, 3]
 
-    # 3. Measure the pixel area of the hand (including the sticker)
-    img_without_bg_alpha = img_without_bg[:, :, 3]
-    hand_num_pixels = np.count_nonzero(img_without_bg_alpha)
+    # 3. Measure the pixel area of the hand (including the sticker).
+    hand_num_pixels = np.count_nonzero(hand_img_without_bg_alpha)
 
-    # 4. Measure the pixel area of the sticker.
-    # --- Red Pixel Filtering using HSV ---
-    # Convert the RGBA image to RGB (necessary for HSV conversion)
-    rgb_image = cv2.cvtColor(img_without_bg, cv2.COLOR_RGBA2RGB)
+    # 4. Find a point inside the sticker.
+    final_mask, sticker_x, sticker_y = find_point_inside_sticker(hand_img_without_bg)
 
-    # Convert the RGB image to HSV
-    hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
+    print(f"Sticker point: ({sticker_x}, {sticker_y})")
 
-    # Define the HSV range for the color red. Red wraps around the 0-180 hue scale in OpenCV.
-    # Lower range (covers bright reds)
-    lower_red = np.array([0, 70, 100])
-    upper_red = np.array([5, 255, 255])
-    mask1 = cv2.inRange(hsv_image, lower_red, upper_red)
+    # 5. Segment the sticker based on the point inside the sticker
+    session = new_session("sam")
+    sam_prompt = [{"type": "point", "data": [sticker_x, sticker_y], "label": 1}]
+    sticker_without_bg_pil = remove(input_image, session=session, sam_prompt=sam_prompt)
+    sticker_without_bg = np.array(sticker_without_bg_pil)
 
-    # Upper range (covers darker, more saturated reds)
-    lower_red2 = np.array([175, 70, 100])
-    upper_red2 = np.array([180, 255, 255])
-    mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+    # # draw a test circle
+    # cv2.circle(sticker_without_bg, (sticker_x, sticker_y), 7, (0, 255, 0), -1)  # Draw a green dot
+    # cv2.putText(sticker_without_bg, f"({sticker_x}, {sticker_y})", (sticker_x - 40, sticker_y - 20),
+    #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    # Combine the two masks to get all red pixels
-    red_hue_mask = mask1 + mask2
+    # 6. Measure the pixel area of the sticker.
+    sticker_without_bg_alpha = sticker_without_bg[:, :, 3]
+    sticker_num_pixels = np.count_nonzero(sticker_without_bg_alpha)
 
-    # Final mask: The pixel must be red AND part of the foreground
-    final_red_mask = cv2.bitwise_and(red_hue_mask, red_hue_mask, mask=img_without_bg_alpha)
-
-    # Count the number of red pixels in the final mask
-    red_filtered_pixels = np.count_nonzero(final_red_mask)
-
-    return final_red_mask, hand_num_pixels
+    return hand_img_without_bg, sticker_without_bg, sticker_num_pixels / hand_num_pixels
 
 with gr.Blocks() as demo:
     gr.Label("Vitiligo Segmentation", container=False)
 
     with gr.Row():
-        hand_img = gr.Image(type="pil", label="Upload a picture of your hand with sticker")
-        result_img = gr.Image(type="pil", label="Result")
+        hand_img = gr.Image(type="pil", label="Upload a picture of your hand with sticker", height=400)
+        # with gr.Group():
+        segmented_hand_img = gr.Image(type="pil", label="Segmented hand", height=400)
+        segmented_sticker_img = gr.Image(type="pil", label="Segmented sticker", height=400)
     with gr.Row():
-        calibrate_btn= gr.Button("Calculate the Sticker/Hand Ratio", variant="primary")
+        calibrate_btn= gr.Button("Calculate the sticker size", variant="primary")
         clear_btn = gr.Button("Clear")
 
-    sticker_hand_ratio = gr.Textbox(label="Sticker/Hand Ratio")
+    sticker_in_hands = gr.Textbox(label="Sticker size in hand units")
 
     clear_btn.click(
         fn=lambda: None,
@@ -93,7 +111,7 @@ with gr.Blocks() as demo:
     calibrate_btn.click(
         fn=calc_sticker_in_hand,
         inputs=hand_img,
-        outputs=[result_img, sticker_hand_ratio],
+        outputs=[segmented_hand_img, segmented_sticker_img, sticker_in_hands],
     )
 
     # with gr.Row():

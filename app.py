74fd0701ae6ae3ef0c60f3fb348abeb04778c6c2
Rayron Victor <rayronaraujo@tjrn.jus.br>
2025-09-15 14:28:53 -0300

    wip

--- app.py
+++ app.py
@@ -26,21 +26,21 @@ def predict_image(img, conf_threshold, iou_threshold):
 
     return im
 
-def find_point_inside_sticker(image):
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
-    hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
+def find_point_inside_sticker(image, color_ranges):
+    if isinstance(image, Image.Image):
+        image = np.asarray(image)
 
-    # Lower red range
-    lower_red1 = np.array([0, 120, 120])
-    upper_red1 = np.array([5, 255, 255])
-    mask1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+    image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
-    # Upper red range
-    lower_red2 = np.array([175, 120, 120])
-    upper_red2 = np.array([180, 255, 255])
-    mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+    final_mask = np.zeros(image.shape[:2], dtype=np.uint8)
 
-    final_mask = mask1 + mask2
+    for color_range in color_ranges:
+        lower_color = np.array(color_range[0])  # [0, 120, 120]
+        upper_color = np.array(color_range[1])  # [5, 255, 255]
+        mask = cv2.inRange(hsv_image, lower_color, upper_color)
+
+        final_mask = final_mask + mask
 
     y_coords, x_coords = np.where(final_mask > 0)
 
@@ -48,6 +48,12 @@ def find_point_inside_sticker(image):
         return final_mask, int(np.mean(x_coords)), int(np.mean(y_coords))
     else:
         return final_mask, 0, 0
+
+def segment_from_point(input_image, x, y):
+    session = new_session("sam")
+    sam_prompt = [{"type": "point", "data": [x, y], "label": 1}]
+    image_without_bg_pil = remove(input_image, session=session, sam_prompt=sam_prompt)
+    return np.array(image_without_bg_pil)
 
 def calc_sticker_in_hand(input_image):
     """
@@ -69,15 +75,26 @@ def calc_sticker_in_hand(input_image):
     hand_num_pixels = np.count_nonzero(hand_img_without_bg_alpha)
 
     # 4. Find a point inside the sticker.
-    final_mask, sticker_x, sticker_y = find_point_inside_sticker(hand_img_without_bg)
+    final_mask, sticker_x, sticker_y = find_point_inside_sticker(
+        hand_img_without_bg,
+        [
+            # Blue range
+            [[100, 80, 80],
+             [140, 200, 200]],
+        ]
+        # [
+        #     # Red range
+        #     [[0, 120, 120], [5, 255, 255]],
+        #     # Upper red range
+        #     [[175, 120, 120], [180, 255, 255]]
+        # ],
+
+    )
 
     print(f"Sticker point: ({sticker_x}, {sticker_y})")
 
     # 5. Segment the sticker based on the point inside the sticker
-    session = new_session("sam")
-    sam_prompt = [{"type": "point", "data": [sticker_x, sticker_y], "label": 1}]
-    sticker_without_bg_pil = remove(input_image, session=session, sam_prompt=sam_prompt)
-    sticker_without_bg = np.array(sticker_without_bg_pil)
+    sticker_without_bg = segment_from_point(input_image, sticker_x, sticker_y)
 
     # # draw a test circle
     # cv2.circle(sticker_without_bg, (sticker_x, sticker_y), 7, (0, 255, 0), -1)  # Draw a green dot
@@ -90,11 +107,29 @@ def calc_sticker_in_hand(input_image):
 
     return hand_img_without_bg, sticker_without_bg, sticker_num_pixels / hand_num_pixels
 
-def calc_vitiligo_areaIn_hand(input_image, sticker_in_hands):
+def calc_vitiligo_area_in_hand(input_image, sticker_in_hands, conf_threshold, iou_threshold):
     """
     desc
     """
-    return [input_image, input_image, sticker_in_hands]
+
+    # 1. Find a point inside the sticker.
+    final_mask, sticker_x, sticker_y = find_point_inside_sticker(
+        np.array(input_image),
+        [
+            # Blue range
+            [[100, 80, 80],
+             [140, 200, 200]],
+        ]
+    )
+
+    im = predict_image(input_image, conf_threshold, iou_threshold)
+
+    print(f"Sticker point: ({sticker_x}, {sticker_y})")
+
+    # 2. Segment the sticker based on the point inside the sticker
+    sticker_without_bg = segment_from_point(input_image, sticker_x, sticker_y)
+
+    return im, input_image, sticker_in_hands
 
 debug = True
 
@@ -125,10 +160,12 @@ with gr.Blocks() as demo:
                 """
             )
             area_img = gr.Image(type="pil", label="Upload a picture of your anatomical area with sticker", height=400)
+            with gr.Accordion("Advanced options", open=False):
+                conf_threshold = gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+                iou_threshold = gr.Slider(minimum=0, maximum=1, value=0.7, label="IoU threshold"),
             calculate_btn = gr.Button("Calculate the vitiligo area", variant="primary")
 
             if debug:
-
                 segmented_area_img = gr.Image(type="pil", label="Segmented anatomical area", height=400)
                 segmented_area_sticker_img = gr.Image(type="pil", label="Segmented sticker", height=400)
         with gr.Column():
@@ -148,8 +185,8 @@ with gr.Blocks() as demo:
     )
 
     calculate_btn.click(
-        fn=calc_vitiligo_areaIn_hand,
-        inputs=[area_img, sticker_in_hands],
+        fn=calc_vitiligo_area_in_hand,
+        inputs=[area_img, sticker_in_hands, conf_threshold, iou_threshold],
         outputs=[segmented_area_img, segmented_area_sticker_img, vitiligo_area_in_hands],
     )
 
